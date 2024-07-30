ccc3ccac61c204242b7b3e7112010f6a0e9ef942
Mika Wang <1721436982@qq.com>
2024-07-30 10:37:44 +0800

    refactor: update router

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -62,21 +62,14 @@ class MyApp extends StatelessWidget {
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
-                  default:
-                    return const SampleItemListView();
-                }
-              },
-            );
+          initialRoute: SampleItemListView.routeName,
+          routes: {
+            SampleItemListView.routeName: (context) =>
+                const SampleItemListView(),
+            SampleItemDetailsView.routeName: (context) =>
+                const SampleItemDetailsView(),
+            SettingsView.routeName: (context) =>
+                SettingsView(controller: settingsController),
           },
         );
       },
